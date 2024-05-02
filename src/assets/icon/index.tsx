a905e0a7f5e04031d300ba1f73906730d8367625
Athaleeah <athakawaii@gmail.com>
2024-05-02 23:16:30 +0800

    add search screen

--- src/assets/icon/index.tsx
+++ src/assets/icon/index.tsx
@@ -5,8 +5,10 @@ import EditIcon from './edit.svg';
 import CameraIcon from './camera.svg';
 import ProfileIcon from './lucide_user-round.svg';
 import Recycle from './recycle.svg';
+import SearchIcon from '../../assets/icon/material-symbols-light_search.svg';
 
 export {
+  SearchIcon,
   Logo,
   BackButton,
   LogoutIcon,
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,5 +4,7 @@ const Waste3 = require('./waste-3.jpg');
 const Waste4 = require('./waste-4.png');
 const Waste5 = require('./waste-5.jpg');
 const Waste6 = require('./waste-6.jpg');
+const Waste7 = require('./waste-7.jpg');
+const Waste8 = require('./waste-8.jpg');
 
-export {Waste1, Waste2, Waste3, Waste4, Waste5, Waste6};
+export {Waste1, Waste2, Waste3, Waste4, Waste5, Waste6, Waste7, Waste8};
