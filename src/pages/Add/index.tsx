76de7dac7d1d74a56b16085cf52da5173891084f
anjelijklmoonik <s2200592@student.unklab.ac.id>
2024-05-12 18:49:14 +0800

    "add" implementation

--- src/pages/Add/index.tsx
+++ src/pages/Add/index.tsx
@@ -7,7 +7,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {NavigationProp} from '@react-navigation/native';
 import {
   BackButton,
@@ -17,17 +17,77 @@ import {
   PlaneIcon,
   SearchCategoryIcon,
 } from '../../assets/icon';
-import {Waste1} from '../../assets/images';
 import {Shadow} from 'react-native-shadow-2';
+import {launchImageLibrary} from 'react-native-image-picker';
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
+import {UserContext} from '../../router';
 
 type AddProps = {
   navigation: NavigationProp<any>;
 };
 
 const Add = ({navigation}: AddProps) => {
+  const user = useContext(UserContext);
+  const [form, setForm] = useState({
+    gambar: null as string | null,
+    kategori: '',
+    keterangan: '',
+    lokasi: '',
+    noTelp: '',
+    status: false,
+    tanggalUpload: new Date(),
+    uid: '',
+  });
   const handleGoBack = () => {
     navigation.goBack();
   };
+
+  const handleInput = (value: string | undefined, input: string) => {
+    setForm({
+      ...form,
+      [input]: value,
+    });
+  };
+
+  const handleImageSelect = async () => {
+    try {
+      const result = await launchImageLibrary({
+        mediaType: 'photo',
+        selectionLimit: 1,
+      });
+      if (result.assets) {
+        handleInput(result.assets[0].uri, 'gambar');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmit = async () => {
+    const filename = form.gambar?.split('/').pop();
+    const reference = storage().ref(filename);
+    try {
+      console.log('fire');
+      await reference.putFile(form.gambar);
+      const url = await reference.getDownloadURL();
+
+      const payload = {
+        ...form,
+        gambar: url,
+        uid: firestore().doc(`user/${user.uid}`),
+      };
+
+      await firestore().collection('sampah').add(payload);
+      handleGoBack();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(form);
+  }, [form]);
   return (
     <View style={{flex: 1}}>
       <Shadow style={styles.header}>
@@ -38,10 +98,35 @@ const Add = ({navigation}: AddProps) => {
           <BackButton />
           <Text style={styles.addText}>Add</Text>
         </TouchableOpacity>
-        <PlaneIcon />
+        <TouchableOpacity onPress={handleSubmit}>
+          <PlaneIcon />
+        </TouchableOpacity>
       </Shadow>
 
-      <Image source={Waste1} style={styles.image} />
+      <View
+        style={{
+          position: 'relative',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          height: 380,
+        }}>
+        {form.gambar ? (
+          <Image source={{uri: form.gambar}} style={styles.image} />
+        ) : (
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              borderWidth: 1,
+              borderRadius: 10,
+              paddingHorizontal: 10,
+              paddingVertical: 5,
+            }}
+            onPress={handleImageSelect}>
+            <Text style={{color: 'black'}}>Browse Image</Text>
+          </TouchableOpacity>
+        )}
+      </View>
 
       <ScrollView contentContainerStyle={{paddingVertical: 20}}>
         <View style={styles.inputContainer}>
@@ -49,6 +134,7 @@ const Add = ({navigation}: AddProps) => {
           <View style={{flex: 1}}>
             <Text style={styles.inputLabel}>Keterangan</Text>
             <TextInput
+              onChangeText={value => handleInput(value, 'keterangan')}
               style={styles.input}
               placeholder="Deskripsikan postingan anda disini..."
               placeholderTextColor={'#A0A0A0'}
@@ -60,6 +146,7 @@ const Add = ({navigation}: AddProps) => {
           <View style={{flex: 1}}>
             <Text style={styles.inputLabel}>Lokasi</Text>
             <TextInput
+              onChangeText={value => handleInput(value, 'lokasi')}
               style={styles.input}
               placeholder="Mencari lokasi..."
               placeholderTextColor={'#A0A0A0'}
@@ -71,6 +158,7 @@ const Add = ({navigation}: AddProps) => {
           <View style={{flex: 1}}>
             <Text style={styles.inputLabel}>No Telp</Text>
             <TextInput
+              onChangeText={value => handleInput(value, 'noTelp')}
               style={styles.input}
               placeholder="Masukan nomor telepon yang akan dihubungi..."
               placeholderTextColor={'#A0A0A0'}
@@ -82,10 +170,28 @@ const Add = ({navigation}: AddProps) => {
           <View style={{flex: 1, rowGap: 20}}>
             <Text style={styles.inputLabel}>Kategori</Text>
             <View style={{flexDirection: 'row', columnGap: 15}}>
-              <TouchableOpacity activeOpacity={0.4} style={styles.button}>
+              <TouchableOpacity
+                activeOpacity={0.4}
+                onPress={() => handleInput('organik', 'kategori')}
+                style={[
+                  styles.button,
+                  {
+                    backgroundColor:
+                      form.kategori === 'organik' ? '#03c2fc' : '#D5F5EA',
+                  },
+                ]}>
                 <Text style={styles.buttonText}>ORGANIK</Text>
               </TouchableOpacity>
-              <TouchableOpacity activeOpacity={0.4} style={styles.button}>
+              <TouchableOpacity
+                activeOpacity={0.4}
+                onPress={() => handleInput('plastik', 'kategori')}
+                style={[
+                  styles.button,
+                  {
+                    backgroundColor:
+                      form.kategori === 'plastik' ? '#03c2fc' : '#D5F5EA',
+                  },
+                ]}>
                 <Text style={styles.buttonText}>PLASTIK</Text>
               </TouchableOpacity>
             </View>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {
   SplashScreen,
@@ -17,8 +17,10 @@ import {
   Add,
 } from '../pages';
 import auth from '@react-native-firebase/auth';
+import {Text, View} from 'react-native';
 
 const Stack = createNativeStackNavigator();
+export const UserContext = createContext<any>(null);
 
 const index = () => {
   const [initializing, setInitializing] = useState(true);
@@ -35,86 +37,96 @@ const index = () => {
     return subscriber; // unsubscribe on unmount
   }, []);
 
+  if (initializing) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <Stack.Navigator>
-      {!user ? (
-        <>
-          <Stack.Screen
-            name="SplashScreen"
-            component={SplashScreen}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="SignIn"
-            component={SignIn}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="SignUp"
-            component={SignUp}
-            options={{headerShown: false}}
-          />
-        </>
-      ) : (
-        <>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Search"
-            component={Search}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Notif"
-            component={Notif}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Upload"
-            component={Upload}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Status"
-            component={Status}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Profile"
-            component={Profile}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="EditProfile"
-            component={EditProfile}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="OrganicRecycle"
-            component={OrganicRecycle}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="PlasticRecycle"
-            component={PlasticRecycle}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="OrganicApproved"
-            component={OrganicApproved}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Add"
-            component={Add}
-            options={{headerShown: false}}
-          />
-        </>
-      )}
-    </Stack.Navigator>
+    <UserContext.Provider value={user}>
+      <Stack.Navigator>
+        {!user ? (
+          <>
+            <Stack.Screen
+              name="SplashScreen"
+              component={SplashScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="SignIn"
+              component={SignIn}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="SignUp"
+              component={SignUp}
+              options={{headerShown: false}}
+            />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Search"
+              component={Search}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Notif"
+              component={Notif}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Upload"
+              component={Upload}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Status"
+              component={Status}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Profile"
+              component={Profile}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="EditProfile"
+              component={EditProfile}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="OrganicRecycle"
+              component={OrganicRecycle}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="PlasticRecycle"
+              component={PlasticRecycle}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="OrganicApproved"
+              component={OrganicApproved}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Add"
+              component={Add}
+              options={{headerShown: false}}
+            />
+          </>
+        )}
+      </Stack.Navigator>
+    </UserContext.Provider>
   );
 };
 
